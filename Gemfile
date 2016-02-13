d0ae023820f8ec297b581e0a0592d93e6637aaad
Jonas Knipper <jonas@robocomobo.com>
2016-02-13 11:54:33 +0100

    Hook up foundation-sites

--- Gemfile
+++ Gemfile
@@ -12,4 +12,4 @@ gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
 gem 'middleman-compass', '>= 4.0.0'
-gem 'middleman-sprockets'
+gem 'middleman-sprockets', github: 'middleman/middleman-sprockets', branch: 'master'
--- config.rb
+++ config.rb
@@ -23,14 +23,14 @@ configure :development do
 end
 
 compass_config do |config|
-  config.add_import_path "bower_components/foundation/scss"
+  config.add_import_path "../bower_components/foundation-sites/assets"
   config.output_style = :compact
 end
 
-after_configuration do
-  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
-      sprockets.append_path File.join "#{root}", @bower_config["directory"]
-end
+#after_configuration do
+  #@bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  ##sprockets.append_path File.join "#{root}", @bower_config["directory"]
+#end
 
 set :haml, { :ugly => true, :format => :html5 }
 
